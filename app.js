a991f84aef9f5460370c514ba944e545c8f5247f
malik LAFIA <maliksero.lafia@gmail.com>
2024-05-02 10:42:33 +0200

    configuration de notre API Rest avec toutes les méthodes CRUD

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
 const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
 const { success } = require("./helper");
+const { getUniqueId } = require("./helper");
 let pokemons = require("./mock-pokemon");
 
 const app = express();
 const port = 3000;
 
-app.use(favicon(__dirname + "/favicon.ico")).use(morgan("dev"));
+app
+  .use(favicon(__dirname + "/favicon.ico"))
+  .use(morgan("dev"))
+  .use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello, Express 2 again !"));
 
@@ -21,6 +26,33 @@ app.get("/api/pokemons/:id", (req, res) => {
 app.get("/api/pokemons", (req, res) => {
   const message = " La liste des pokémons a bien été récupérée.";
   res.json(success(message, pokemons));
+});
+
+app.post("/api/pokemons", (req, res) => {
+  const id = getUniqueId(pokemons);
+  const pokemonCreated = { ...req.body, ...{ id: id, created: new Date() } };
+  pokemons.push(pokemonCreated);
+  const message = `Le pokemon ${pokemonCreated.name} a bien été crée.`;
+  res.json(success(message, pokemonCreated));
+});
+
+app.put("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemonUpdated = { ...req.body, id: id };
+  pokemons = pokemons.map((pokemon) => {
+    return pokemon.id === id ? pokemonUpdated : pokemon;
+  });
+
+  const message = `Le pokemon ${pokemonUpdated.name} a bien été modifié.`;
+  res.json(success(message, pokemonUpdated));
+});
+
+app.delete("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemonDeleted = pokemons.find((pokemon) => pokemon.id === id);
+  pokemons = pokemons.filter((pokemon) => pokemon.id !== id);
+  const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`;
+  res.json(success(message, pokemonDeleted));
 });
 
 app.listen(port, () =>
